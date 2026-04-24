07caba85858e9566dd22a01d907ac21d74598341
Admin <Admin@DESKTOP-VI9VDJ6>
2026-04-24 19:00:46 +0300

    создал класс для корзины CartItem

---
+++ WPF/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF
+{
+    public class CartItem
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+
+
+        public Products Product { get; set; }
+    }
+}
